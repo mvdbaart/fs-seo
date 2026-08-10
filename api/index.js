73588a9729fa6f760d3ccbf1fd2a3102c259c901
mvdbaart <mvdbaart@gmail.com>
2026-08-10 11:11:18 +0200

    Capture Vercel serverless init errors and update vercel.json destination

--- api/index.js
+++ api/index.js
@@ -1,3 +1,20 @@
-const app = require('../server/index.js');
+let app;
+let initError = null;
 
-module.exports = app;
+try {
+  app = require('../server/index.js');
+} catch (err) {
+  console.error('[vercel-api] Init error:', err);
+  initError = err;
+}
+
+module.exports = (req, res) => {
+  if (initError) {
+    res.status(500).json({
+      error: 'Backend Fout op Vercel: ' + initError.message,
+      stack: initError.stack
+    });
+    return;
+  }
+  return app(req, res);
+};
